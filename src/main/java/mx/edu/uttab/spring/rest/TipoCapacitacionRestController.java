c82fa8f996bb41c688bc787b4c8c361a46db8d88
WEBXICO-MX <weiss.uttab@gmail.com>
2016-03-15 10:47:46 -0600

    Se le retiran  imports innecesarios.

--- src/main/java/mx/edu/uttab/spring/rest/TipoCapacitacionRestController.java
+++ src/main/java/mx/edu/uttab/spring/rest/TipoCapacitacionRestController.java
@@ -11,13 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.AbstractJsonpResponseBodyAdvice;
-
 import mx.edu.uttab.spring.model.TipoCapacitacion;
 import mx.edu.uttab.spring.service.TipoCapacitacionService;
 
